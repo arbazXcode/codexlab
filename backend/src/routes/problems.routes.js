0a4b5e172137bff4ccffe6604694473e06d206ad
arbazXcode <jmi.arbaz1@gmail.com>
2026-03-07 03:25:15 +0530

    feat(problem): add admin protected problem creation endpoint

--- backend/src/routes/problems.routes.js
+++ backend/src/routes/problems.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { authenticate, checkAdmin } from "../middleware/auth.middleware.js"
-import { createProblem } from "../controllers/problems.controller.js";
+import { createProblem } from "../controllers/problems.controllers.js";
 
 const problemRoutes = Router();
 
-problemRoutes.post("/create-problem", authenticate, checkAdmin, createProblem)
+problemRoutes.post("/problem", authenticate, checkAdmin, createProblem)
 
 export default problemRoutes;
